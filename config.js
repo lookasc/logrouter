1a5a9f028c6099e0085a6691abf3187ca27adc0b
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-11-26 13:48:07 +0100

    Create FileCoder class and refactor dospatcher

--- config.js
+++ config.js
@@ -8,10 +8,13 @@ module.exports = {
 		DIR: process.env.STORE_DIR || './data/',
 		ACTIVE_BUFFER_FILE_EXTENSION: 'active',
 		INACTIVE_BUFFER_FILE_EXTENSION: 'stored',
+		ENCRYPTED_BUFFER_FILE_EXTENSION: 'encrypted',
 		ACTIVE_BUFFER_MAX_SIZE: process.env.ACTIVE_BUFFER_MAX_SIZE || '64k'
 	},
 	ENCRYPT: {
 		ENABLED: process.env.ENCRYPT_DATA || true,
-		PASSWORD: process.env.ENCRYPT_PASSWORD || 'testPass'
+		PASSWORD: process.env.ENCRYPT_PASSWORD || 'testPass',
+		PASSWORD_HASH_ALGHORITM: 'sha256',
+		ALGORITHM: 'aes-256-cbc'
 	}
 };
--- dispatcher/dispatcher.js
+++ dispatcher/dispatcher.js
@@ -1,10 +1,12 @@
 const { createReadStream, createWriteStream, rename } = require('fs');
-const readline = require('readline');
-const { ENCRYPT } = require('../config');
+const { createInterface } = require('readline');
+const { ENCRYPT, FILES } = require('../config');
 const crypto = require('crypto');
 
-const algorithm = 'aes-256-cbc';
-const key = crypto.createHash('sha256').update(ENCRYPT.PASSWORD).digest();
+const key = crypto
+	.createHash(ENCRYPT.PASSWORD_HASH_ALGHORITM)
+	.update(ENCRYPT.PASSWORD)
+	.digest();
 
 process.send('Dispatcher started');
 
@@ -16,32 +18,46 @@ function parseMessage(message) {
 	if (!message || !message.type) return;
 
 	if (message.type === 'newFile') {
-		encryptFile(message.fileName);
+		new FileCoder(message.fileName)
+			.encryptFile()
+			.then(encryptedFileName => {
+				process.send(`Encrypting of ${encryptedFileName} finished`);
+			});
 	}
 }
 
-function encryptFile(fileName) {
-	let rl = readline.createInterface({
-		input: createReadStream(fileName),
-		crlfDelay: Infinity
-	});
 
-	let writeStream = createWriteStream(fileName + '.encrypted');
-	
-	rl.on('line', (line) => {
-		writeStream.write(`${JSON.stringify(encrypt(line))}\n`);
-	});
+class FileCoder {
+
+	constructor(fileName) {
+		this.inputFileName = fileName;
+		this.encryptedFileName = `${this.inputFileName}.${FILES.ENCRYPTED_BUFFER_FILE_EXTENSION}`;
+		this.encryptedFileStream = createWriteStream(this.encryptedFileName);
+	}
+
+	encryptFile() {
+		let lineByLineReader = createInterface({
+		input: createReadStream(this.inputFileName),
+		crlfDelay: Infinity
+		});
+
+		lineByLineReader.on('line', (line) => {
+			this.encryptedFileStream.write(`${this.encrypt(line)}\n`);
+		});
+
+		return new Promise((resolve, reject) => {
+			lineByLineReader.on('close', () => {
+				resolve(this.encryptedFileName);
+			});
+		});
+	}
+
+	encrypt(text) {
+		const iv = crypto.randomBytes(16);
+		let cipher = crypto.createCipheriv(ENCRYPT.ALGORITHM, Buffer.from(key), iv);
+		let encrypted = cipher.update(text);
+		encrypted = Buffer.concat([encrypted, cipher.final()]);
+		return `${iv.toString('hex')}${encrypted.toString('hex')}`;
+	}
 
 }
-
-function encrypt(text) {
-	const iv = crypto.randomBytes(16);
-	let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-	let encrypted = cipher.update(text);
-	encrypted = Buffer.concat([encrypted, cipher.final()]);
-	return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
- }
-
-
-
-
