bc1c0f3eca0728fb4cede0b7683c1bf2f511cac0
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-11-27 23:40:47 +0100

    Add Buffer class tests

---
+++ tests/collector/index.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Collector = require('../../src/collector');
+
+// describe('Collector class', () => {
+// 	let collector;
+
+// 	beforeEach(() => {
+// 		collector = new Collector();
+// 	});
+
+// 	it('should create instance od Collector', () => {
+// 		expect(collector).to.be.instanceOf(Collector);
+// 	});
+
+// });
