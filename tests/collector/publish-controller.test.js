06ccb88de80a305b202ca1dc38cb6b8f27e37c41
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-11-28 23:17:45 +0100

    Add test for PublishController class

---
+++ tests/collector/publish-controller.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const PublishController = require('../../src/collector/publish-controller');
+
+describe('PublishController', () => {
+	let publishController;
+
+	beforeEach(() => {
+		publishController = new PublishController();
+	});
+
+	it('should create instance of PublishController', () => {
+		expect(publishController).to.be.instanceOf(PublishController);
+	});
+
+});
