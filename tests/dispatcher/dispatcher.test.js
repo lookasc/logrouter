c003dc86a9ba821f47a6c174f3731391ca242a15
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-12-03 22:18:15 +0100

    Add Dispatcher class tests

--- tests/dispatcher/dispatcher.test.js
+++ tests/dispatcher/dispatcher.test.js
@@ -4,15 +4,29 @@ const fs = require('fs');
 const { FILES } = require('../../config');
 const Dispatcher = require('../../src/dispatcher/dispatcher');
 
-const dummyUdpClient = {};
+const dummyUdpClient = {
+	sentLines: 0,
+	isLocked: false,
+	send: function (line) {
+		this.sentLines++;
+	},
+	lockInput: function () {
+		this.isLocked = true;
+	},
+	onAllItemsSent: function (callback) {
+		setTimeout(() => {
+			callback();
+		}, 5);
+	}
+};
 
 describe('Dispatcher class', () => {
 	let dispatcher;
-	let dummyData = 'dummyData';
+	let dummyData = 'dummyData\ndummyData';
 	let dummyName;
 
 	beforeEach(() => {
-		dummyName = FILES.DIR + (Math.floor(Math.random()*1000000)) + '.' + FILES.INACTIVE_BUFFER_FILE_EXTENSION;
+		dummyName = FILES.DIR + (Math.floor(Math.random()*1000000)) + '.' + FILES.ENCRYPTED_BUFFER_FILE_EXTENSION;
 		fs.writeFileSync(dummyName, dummyData);
 
 		dispatcher = new Dispatcher({
@@ -23,6 +37,16 @@ describe('Dispatcher class', () => {
 
 	it('should create instance od FileCoder', () => {
 		expect(dispatcher).to.be.instanceOf(Dispatcher);
+	});
+
+	it('should send 2 lines of data and resolve with file name', (done) => {
+		dispatcher.sendPartedFile()
+			.then((encryptedFileName) => {
+				expect(encryptedFileName).to.equal(dummyName);
+				expect(dispatcher.udpClient.sentLines).to.equal(2);
+				expect(dispatcher.udpClient.isLocked).to.be.true;
+				done();
+			});
 	});
 
 });
