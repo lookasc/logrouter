2d5889b11f8f786e98f5c28dde8139e884f9355d
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-12-04 21:46:57 +0100

    Add tests for whole dispatcher

--- tests/dispatcher/index.test.js
+++ tests/dispatcher/index.test.js
@@ -1,36 +1,33 @@
 const chai = require('chai');
 const expect = chai.expect;
+const { FILES } = require('../../config');
 const { fork } = require('child_process');
-const { existsSync } = require('fs');
+const { existsSync, writeFileSync } = require('fs');
 
 describe('Whole dispatcher test', () => {
 	let dispatcher;
 	let testFileName;
+	let dummyData = 'dummyData\ndummyData\ndummyData\ndummyData\n';
 
 	before(() => {
-		let fileName = FILES.DIR + (Math.floor(Math.random()*1000000)) + '.' + FILES.ENCRYPTED_BUFFER_FILE_EXTENSION;
-		let dummyData = 'dummyData\ndummyData\ndummyData\ndummyData\n';
-		writeFileSync(fileName, dummyData);
+		testFileName = FILES.DIR + (Math.floor(Math.random()*1000000)) + '.' + FILES.ENCRYPTED_BUFFER_FILE_EXTENSION;
+		writeFileSync(testFileName, dummyData);
 		dispatcher = fork('./src/dispatcher');
 	});
 
-	// beforeEach((done) => {
-	// 	testProcess.send('createAndSendTestFile');
-	// 	setTimeout(() => {
-	// 		done();
-	// 	}, 500);
-	// });
-
 	it('should create test file', () => {
-		expect(existsSync(testFileName)).to.be.true;
+		setTimeout(() => {
+			expect(existsSync(testFileName)).to.be.true;
+		}, 50);
 	});
 
 	it('should delete processed file after dispatch', () => {
-		expect(existsSync(testFileName)).to.be.true;
+		dispatcher.send(testFileName);
+		setTimeout(() => {
+			expect(existsSync(testFileName)).to.be.false;
+		}, 500);
 	});
 
-
-	
 	after(() => {
 		dispatcher.kill('SIGINT');
 	});
