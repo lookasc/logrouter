4472dedc3da8d130cff62271c3a36e879030c436
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-12-14 23:35:21 +0100

    Integrate 'filebuffer' package

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
-const DISPATCHER_PATH = './src/dispatcher/';
-const PublishController = require('./publish-controller');
-const BufferController = require('./buffer-controller');
-const CollectorServer = require('./collector-server');
+const { FileBuffer } = require('filebuffer');
+const DISPATCHER_PATH = './src/dispatcher';
+const PublishController = require('./src/collector/publish-controller');
+const CollectorServer = require('./src/collector/collector-server');
 const Collector = require('./src/collector/collector');
 
 var publishController = new PublishController({
-	dispatherPath: DISPATCHER_PATH
+	dispatcherPath: DISPATCHER_PATH
 });
 
-var bufferController = new BufferController({
-	publishController: publishController
+var bufferController = new FileBuffer({
+	activeBufferMaxSize: '1M',
+	allowTimeRollover: false
+});
+
+bufferController.on('bufferExchange', e => {
+  publishController.dispatch(e.fileName);
 });
 
 var collectorServer = new CollectorServer({
