2fca94296e3e2c54ea17bc7cb968960f968071bd
Łukasz Ciesielski <ciesiel555@gmail.com>
2020-01-29 14:00:01 +0100

    Delete useless Collector class

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const { FileBuffer } = require('filebuffer');
 const DISPATCHER_PATH = './src/dispatcher';
 const PublishController = require('./src/collector/publish-controller');
 const CollectorServer = require('./src/collector/collector-server');
-const Collector = require('./src/collector/collector');
 
 var publishController = new PublishController({
 	dispatcherPath: DISPATCHER_PATH
@@ -20,8 +19,4 @@ bufferController.on('bufferExchange', e => {
 
 var collectorServer = new CollectorServer({
 	bufferController: bufferController
-});
-
-new Collector({
-	server: collectorServer
 });
